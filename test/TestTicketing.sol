628b4494623426c7f0d29996f3ebe3b5d4f9a2c3
MrGuacamole <37054689+MrGuacamole@users.noreply.github.com>
2019-01-27 18:00:50 +1300

    Add comments

--- test/TestTicketing.sol
+++ test/TestTicketing.sol
@@ -7,17 +7,20 @@ import "../contracts/Ticketing.sol";
 contract TestTicketing {
  // The address of the ticketing contract to be tested
  Ticketing ticketing = Ticketing(DeployedAddresses.Ticketing());
-
+ 
+ // This tests whether the correct amount of tickets are remaining after the 
+ // contract is deployed. It is important to ensure that the contract begins with some amount 
+ // of tickets to purchase.
  function testTicketsRemaining() public {
  	 uint expected = 1000;
 
     Assert.equal(ticketing.remainingTickets(), expected, "Tickets remaining should initially be 1000");
  }
-
+// This tests if the circuit breaker is initally inactive. This ensures that the contract is in the right 
+// state on creation and tickets can be purchased.
  function testBreaker() public{
  	bool expected = false;
 
  	Assert.equal(ticketing.stopExecution(), expected, "Breaker should start inactive");
- }
- 
+ } 
 }
